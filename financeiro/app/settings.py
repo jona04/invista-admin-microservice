c08c67fbda9e49e1184e388ed3b7e42b89c987d9
Jonatas O <jonatas.oliveira@optimumsupply.com.br>
2022-11-16 17:58:34 -0300

    edit host database microservice to rds

--- financeiro/app/settings.py
+++ financeiro/app/settings.py
@@ -82,18 +82,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": os.getenv('DB_DATABASE'),  # Or path to database file if using sqlite3.
+        "NAME": "invista_financeiro",  # Or path to database file if using sqlite3.
         "USER": os.getenv('DB_USERNAME'),
         "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        "PORT": os.getenv('DB_PORT'),  # Set to empty string for default.
-    },
-    "old": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": "ddovnp4rmcdodh",  # Or path to database file if using sqlite3.
-        "USER": "ptxyfilpuuzbxb",
-        "PASSWORD": "034b98845ac9e1df1eaeac0abac1bc0bfe4db1080bf1faaec2e0d275fd15dde8",
-        "HOST": "ec2-3-213-134-196.compute-1.amazonaws.com",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "HOST": "invista-financeiro-db.cr6ld9unj4h4.us-east-1.rds.amazonaws.com",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         "PORT": os.getenv('DB_PORT'),  # Set to empty string for default.
     }
 }
--- users/app/settings.py
+++ users/app/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        "NAME": os.getenv('DB_DATABASE'),  # Or path to database file if using sqlite3.
+        "NAME": "invista_users",  # Or path to database file if using sqlite3.
         "USER": os.getenv('DB_USERNAME'),
         "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "HOST": "invista-users-db.cr6ld9unj4h4.us-east-1.rds.amazonaws.com",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         "PORT": os.getenv('DB_PORT'),  # Set to empty string for default.
     }
 }
